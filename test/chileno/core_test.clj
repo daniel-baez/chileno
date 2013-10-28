846735c32b97c6d4c88e2908a8935d2c0e17be78
Sergio Campamá <sergiocampama@gmail.com>
2013-10-27 22:15:26 -0300

    More simple conditions on verifier digit. `dv.make` now accepts numbers instead of only strings.

--- test/chileno/core_test.clj
+++ test/chileno/core_test.clj
@@ -1,6 +1,7 @@
 (ns chileno.core-test
   (:require [clojure.test :refer :all]
-            [chileno.core :as rut]))
+            [chileno.core :as rut]
+            [chileno.dv :as dv]))
 
 (deftest clean
   (is (= "123456789"
@@ -11,3 +12,7 @@
 (deftest k-dv
   (is (rut/valid? "123456785-k"))
   (is (rut/valid? "123456785-K")))
+
+(deftest numbers
+  (is (= (dv/make 1) "9"))
+  (is (= (dv/make 23) "K")))
